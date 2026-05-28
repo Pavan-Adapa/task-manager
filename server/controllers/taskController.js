16de5c3c8d4502942230bb4b29d3616b253f7879
Pavan-Adapa <padapa102@gmail.com>
2026-05-28 08:41:19 +0530

    Added task CRUD APIs

---
+++ server/controllers/taskController.js
@@ -0,0 +1,106 @@
+const Task = require("../models/Task");
+
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({
+      user: req.user._id,
+    });
+
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const createTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+
+    if (!title) {
+      return res.status(400).json({
+        message: "Title is required",
+      });
+    }
+
+    const task = await Task.create({
+      title,
+      description,
+      user: req.user._id,
+    });
+
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    if (task.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        message: "Not authorized",
+      });
+    }
+
+    task.title = req.body.title || task.title;
+
+    task.description = req.body.description || task.description;
+
+    task.status = req.body.status || task.status;
+
+    const updatedTask = await task.save();
+
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    if (task.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        message: "Not authorized",
+      });
+    }
+
+    await task.deleteOne();
+
+    res.json({
+      message: "Task deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  getTasks,
+  createTask,
+  updateTask,
+  deleteTask,
+};
--- server/server.js
+++ server/server.js
@@ -2,12 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
+const connectDB = require("./config/db");
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/test", require("./routes/testRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
